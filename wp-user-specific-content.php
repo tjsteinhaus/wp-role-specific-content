cc1662c949d15c15f07dacc79c964bc013a9260d
Tyler Steinhaus <tyler.steinhaus@constellationmutual.com>
2018-07-31 14:05:08 -0500

    Moved away from custom autoload and switched to composer.

--- wp-user-specific-content.php
+++ wp-user-specific-content.php
@@ -13,31 +13,8 @@ namespace WPRoleSpecificContent;
 
 define( 'WPRoleSpecificContent_DIR', plugin_dir_path( __FILE__ ) );
 
-/**
-  * Create our Autoload so we don't have to include files.
-  */
-spl_autoload_register( function( $class ) {
-    $class = ltrim( $class, '\\' );
-
-    if( strpos( $class, __NAMESPACE__ ) !== 0 ) return;
-
-    // modify the file structure to support lowercase folders
-    $class = str_replace( __NAMESPACE__, '', $class );
-    $path = str_replace( '\\', DIRECTORY_SEPARATOR, $class );
-    $split_path = explode( DIRECTORY_SEPARATOR, $class );
-    if( count( $split_path ) > 2 ) {
-        $file_name = $split_path[count($split_path)-1];
-        unset( $split_path[count($split_path)-1] );
-        $split_path = array_map( 'strtolower', $split_path );
-        $path = implode( DIRECTORY_SEPARATOR, $split_path );
-
-        $path = __DIR__ . DIRECTORY_SEPARATOR . 'src' . $path . DIRECTORY_SEPARATOR . $file_name . '.php';
-    } else {
-        $path = __DIR__ . DIRECTORY_SEPARATOR . 'src' . $path . '.php';
-    } 
-
-    require_once( $path );
-} );
+// use Composer autoload
+require __DIR__ . '/vendor/autoload.php';
 
 add_action( 'init', function() {
 	// Start the engines
